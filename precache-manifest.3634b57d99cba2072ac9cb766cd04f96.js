cb76200d67a0d9937096bea35a7a960c4b753e56
miteneva.sofya.99 <miteneva.sofya.99@mail.ru>
2025-01-29 17:09:01 +0300

    Add files via upload

---
+++ precache-manifest.3634b57d99cba2072ac9cb766cd04f96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984789e887ff1191836808352d870eec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aebaead41ba14c541b92",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "fa2d8ee02cf184fc4314",
+    "url": "/static/js/2.3ad75f56.chunk.js"
+  },
+  {
+    "revision": "aebaead41ba14c541b92",
+    "url": "/static/js/main.973979f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
